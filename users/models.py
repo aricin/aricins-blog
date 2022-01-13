67b881e1a281e55a67f52bf798ca5f315a1744c0
Aricin Rotarius <aricin.rotarius@gmail.com>
2022-01-12 22:01:16 -0600

    Add darkmode feature

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     bio = models.CharField(default='', max_length=30)
+    darkmode = models.BooleanField(default=False)
 
         # def save(self, *args, **kwargs):
         #     super().save(*args, **kwargs)
